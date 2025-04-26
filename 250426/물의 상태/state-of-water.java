8f808aaaed8d22ac9954ba4ed0b8a1c05189b300
백채연 <163715888+Chaeyeon0@users.noreply.github.com>
2025-04-26 23:06:42 +0900

    Title: 물의 상태, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 250426/물의 상태/state-of-water.java
+++ 250426/물의 상태/state-of-water.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int temperature = sc.nextInt();  // -200 ~ 200 사이 값이 들어옴
+        int temperature = sc.nextInt();  
 
-        if (temperature <= 0) {
+        if (temperature < 0) {
             System.out.println("ice");
         } else if (temperature >= 100) {
             System.out.println("vapor");
