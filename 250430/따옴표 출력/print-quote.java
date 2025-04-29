0f76afd73b7687b7f67adf166a592b573d8935d2
백채연 <163715888+Chaeyeon0@users.noreply.github.com>
2025-04-30 00:21:45 +0900

    Title: 따옴표 출력, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250430/따옴표 출력/print-quote.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = "He says \"It's a really simple sentence\".";
+        System.out.println(message);
+    }
+}
