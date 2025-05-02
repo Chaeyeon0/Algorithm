d5c10f438da91f61cbb19a5816347259598db61b
백채연 <163715888+Chaeyeon0@users.noreply.github.com>
2025-05-03 02:04:09 +0900

    Title: 친근하지 않은 수, Time: 114ms, Memory: 10MB, Status: Passed - Codetree

--- 250503/친근하지 않은 수/unfriendly-number.java
+++ 250503/친근하지 않은 수/unfriendly-number.java
@@ -6,12 +6,14 @@ public class Main {
         int N = sc.nextInt();
         int count = 0;
 
-        for (int i = 1; i < N; i++) {
-            if (i%2==0 || i%3==0 || i%5==0) {
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {
                 continue;
             }
+            // 친근하지 않은 수면 count 증가
             count++;
         }
+
         System.out.println(count);
     }
 }
